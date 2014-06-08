8bf39ec628d49383b1cbe4d27651f25a0aea5b77
Dave Riddle <dave@mywebroom.com>
2014-06-08 13:48:39 -0700

    adds host param (for devise)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -33,6 +33,9 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+
+  # Host
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Host
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
