c599ae53213ef2ed5332e7fd74e4752f65b1c134
Dave Riddle <dave@mywebroom.com>
2014-06-08 11:50:07 -0700

    only allow swansway unique swansway signups

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -25,6 +25,10 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  # VALIDATIONS
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@swansway.com\z/i }
 end
