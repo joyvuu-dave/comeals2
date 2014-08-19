cea57279f72f76674e4a9f52f5ec594e68b9909c
Dave Riddle <david@joyvuu.com>
2014-08-19 02:54:20 -0700

    remove migration that's now failing

--- db/migrate/20140819061342_remove_foreign_keys.rb
+++
@@ -1,9 +0,0 @@
-class RemoveForeignKeys < ActiveRecord::Migration
-  def change
-    remove_foreign_key :bills, name: 'bills_meal_id_fk'
-    remove_foreign_key :bills, name: 'bills_resident_id_fk'
-    remove_foreign_key :meal_residents, name: 'meal_residents_meal_id_fk'
-    remove_foreign_key :meal_residents, name: 'meal_residents_resident_id_fk'
-    remove_foreign_key :residents, name: 'residents_unit_id_fk'
-  end
-end
